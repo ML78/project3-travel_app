888ea79f953ccd3333dd3d7206e8f2c944738be7
Monique Lee <moniquelee.7878@gmail.com>
2018-05-22 15:48:29 +1000

    update nav, styles

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,7 +1,7 @@
 class LocationsController < ApplicationController
   before_action :set_location, only: [:show, :edit, :update, :destroy]
 
-  before_action :require_user, except: [:index, :show]
+  before_action :require_user, except: [:show]
   before_action :require_admin, only: [:create, :edit, :update, :destroy]
 
   # GET /locations
